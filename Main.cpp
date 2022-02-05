25583eacfeab067724b57efd52db6778705503ee
masonself <masonselfmusic@gmail.com>
2022-02-04 21:53:58 -0500

    Complete Instuctions

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,56 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <regex>
+using namespace std;
+
 int main()
 {
+	//Part 1 of Assignment 1 BEGIN
+	regex validIntegerSyntax("\-?\\d+");
+	string userIntInput;
+	int intValue;
+
+	regex validDoubleSyntax("\-?\\d+[.]\\d+");
+	string userDoubleInput;
+	double doubleValue;
+	
+	string userStringInput;
+
+	for (;;)
+	{
+		cout << "Pleae enter an integer: ";
+		cin >> userIntInput;
+		if (!regex_match(userIntInput, validIntegerSyntax))
+		{
+			cout << "Invalid input: not an integer.\n";
+			continue;
+		}
+		intValue = stoi(userIntInput, nullptr, 10);
+		break;
+	}
+	for (;;)
+	{
+		cout << "Please enter a double: ";
+		cin >> userDoubleInput;
+		if (!regex_match(userDoubleInput, validDoubleSyntax))
+		{
+			cout << "Invalid input: not an double.\n";
+			continue;
+		}
+		doubleValue = stod(userDoubleInput, nullptr);
+		break;
+	}
+	cout << "Please enter a string: ";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	getline(cin, userStringInput);
+
+	cout << "User Int Input: " << intValue << endl;
+	cout << "User Double Input: " << doubleValue << endl;
+	cout << "User String Input: " << userStringInput << endl;
+	cout << "\n-----------------------\n\n";
+	//Part 1 of Assignment 1 END
+	
 	// random number generator used in some tests
 	srand(::time_t(NULL));
 
--- StringSteamTest.cpp
+++ StringSteamTest.cpp
@@ -1,46 +1,38 @@
 #include "TestHarness.h"
 
-// To add your own unit tests
-// 
-// 1) Rename this file using a name that describes the software entity under test. E.g., if you were testing std::vector
-//    rename this file as VectorTest.cpp
-// 2) Add additional test files as needed. E.g., if you were testing a class named Animal and another class named Zoo you
-//    would have two test files. AnimalTest.cpp and ZooTest.cpp
-// 3) Remove the sample tests from this file
-
 #include <string>
-#include <iostream>
+using namespace std;
 
 TEST(doubleToStream, stringstream)
 {
-    std::stringstream message; 
+    stringstream message; 
     message << 5.0;
     double value;
     message >> value;
-    CHECK_DOUBLES_EQUAL(5.0, value, std::numeric_limits<double>::epsilon())
+    CHECK_DOUBLES_EQUAL(5.0, value, numeric_limits<double>::epsilon())
 }
 
 TEST(floatToStream, stringstream)
 {
-    std::stringstream message;
+    stringstream message;
     message << 5.0f;
     float value;
     message >> value;
-    CHECK_DOUBLES_EQUAL(5.0f, value, std::numeric_limits<float>::epsilon())
+    CHECK_DOUBLES_EQUAL(5.0f, value, numeric_limits<float>::epsilon())
 }
 
 TEST(stringToStream, stringstream)
 {
-    std::stringstream message;
+    stringstream message;
     message << "Hello";
-    std::string testString;
+    string testString;
     message >> testString;
     CHECK_EQUAL("Hello", testString)
 }
 
 TEST(streamToIntFailure, stringstream)
 {
-    std::stringstream message("Hello");
+    stringstream message("Hello");
     int testInt;
     message >> testInt;
     if (!message)
@@ -48,6 +40,6 @@ TEST(streamToIntFailure, stringstream)
     }
     else
     {
-        CHECK_FAIL("Streaming of String to Int DID NOT fail but it should have");
+        CHECK_FAIL("Streaming of string to int DID NOT fail but it should have");
     }   
 }
